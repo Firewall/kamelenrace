7b88b188a97b9a6065f28f4825478195dd99c71c
Robin Geldolf <robin.geldolf@kahosl.be>
2013-10-30 12:35:23 +0100

    poging 2

---
+++ collections/databases.js
@@ -0,0 +1,2 @@
+Players = new Meteor.Collection('players');
+Messages = new Meteor.Collection("messages") ;
---
+++ server/servercode.js
@@ -0,0 +1,6 @@
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    Messages.remove({});
+    // code to run on server at startup
+  });
+}
