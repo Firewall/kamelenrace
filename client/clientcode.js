ba278984a0c5d02ee5fdd75483ca70e310f22b2f
Ammoboy <ewout.merckx@kahosl.be>
2013-12-01 18:02:03 +0100

    Holes are added, the ball is added and you can drag the ball

--- client/clientcode.js
+++ client/clientcode.js
@@ -117,6 +117,59 @@ if (Meteor.isClient) {
     })
 
 
+// ----------------------------------------------------------------------------
+// All animation for the ballThrowLocation
+
+    // the code for drawing a hole at the top
+    function drawHole(x, y, r, number){
+        var hole = snapBall.circle(x, y, r);
+        hole.attr({
+            fill: '#C7C7C7',
+            stroke: '#000',
+            strokeWidth: '3'
+        });
+        var text = snapBall.text(x - 7, y + 10, number);
+        text.attr({
+            fill: '#fff',
+            'font-size': '30px',
+            'font-weight': 'bold'
+        });
+        return hole;
+    }
+
+
+    // we are going to animate the ball
+    // after the element has been rendered
+    Template.ball.rendered = function() {
+
+        //define some all used variables
+        var ballFieldWidth = $('#ballTrowLocation').width();
+        var ballWidth = 20;
+
+        //select the canvas to add snaps
+        snapBall = Snap('#ballTrowLocation');
+
+        //we make the holes at the top
+        var hole1 = drawHole(ballFieldWidth / 2, 150, ballWidth + 20, '1')
+        var hole2 = drawHole(ballFieldWidth / 2 - 2.5 * ballWidth, 90, ballWidth + 15, '2')
+        var hole3 = drawHole(ballFieldWidth / 2 + 2.5 * ballWidth, 90, ballWidth + 11, '3')
+        var hole4 = drawHole(ballFieldWidth / 2 + 5 * ballWidth, 50, ballWidth + 7, '4')
+        var hole5 = drawHole(ballFieldWidth / 2 - 5 * ballWidth, 50, ballWidth + 4, '5')
+
+        //we create a circle
+        var userCircle = snapBall.circle(ballFieldWidth / 2, 600, ballWidth);
+
+        //we give the circle the color of red
+        userCircle.attr({
+            fill: 'r()#FF0000-#B30000',//'FF0000''#D40404',
+            stroke: '#000',
+            strokeWidth: '1'
+        })
+
+        userCircle.drag();
+    };
+
+
 
 
 
